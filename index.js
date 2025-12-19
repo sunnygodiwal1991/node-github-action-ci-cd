df85563082d611a69bf36929216d9f29c8eac9f1
sunnygodiwal1991 <60594463+sunnygodiwal1991@users.noreply.github.com>
2025-12-19 13:07:55 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('🚀 Node.js App successfully deployed on EKS!');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
